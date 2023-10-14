5359cdb8f44c74dbac525c1116b0fa0557039d8a
Paul W Saltzman <131216446+Paul-W-Saltzman@users.noreply.github.com>
2023-10-14 11:46:36 -0500

    Still trying to get this to work.

--- Flashcards/Data.cs
+++ Flashcards/Data.cs
@@ -1,40 +1,81 @@
 ﻿using System;
 using Microsoft.Data.SqlClient;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Flashcards
 {
-    internal class Program
+    internal class Data
     {
-        private static readonly string connectionString = "Server=localhost;Integrated Security=SSPI;";
+        private static readonly string connectionString = "" +
+            "Data Source=localhost;" +
+            "Integrated Security = True; Connect Timeout = 30;" +
+            " Encrypt=False;" +
+            "Trust Server Certificate=False;" +
+            "Application Intent = ReadWrite; " +
+            "Multi Subnet Failover=False";
 
-        private static void Main(string[] args)
+        internal static void CheckCreateDatabase()
         {
+
+            string databaseName = "FlashCards";
+
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    string createDatabaseQuery = "CREATE DATABASE MyDatabase ON PRIMARY " +
-                                                "(NAME = MyDatabase_Data, " +
-                                                "FILENAME = 'C:\\MyDatabaseData.mdf', " +
-                                                "SIZE = 2MB, MAXSIZE = 10MB, FILEGROWTH = 10%)" +
-                                                "LOG ON (NAME = MyDatabase_Log, " +
-                                                "FILENAME = 'C:\\MyDatabaseLog.ldf', " +
-                                                "SIZE = 1MB, " +
-                                                "MAXSIZE = 5MB, " +
-                                                "FILEGROWTH = 10%)";
+                    // Check if the database already exists
+                    bool databaseExists = CheckIfDatabaseExists(connection, databaseName);
 
-                    using (SqlCommand command = new SqlCommand(createDatabaseQuery, connection))
+                    if (databaseExists)
                     {
-                        connection.Open();
-                        command.ExecuteNonQuery();
-                        Console.WriteLine("Database is created successfully.");
+                        Console.WriteLine("Database already exists.");
+                        Console.ReadLine();
+                    }
+                    else if(!databaseExists)
+                    {
+                        // Create the database
+
+                        string createDatabaseQuery = $"CREATE DATABASE {databaseName} ON PRIMARY " +
+                                                    $"(NAME = {databaseName}_Data, " +
+                                                    $"FILENAME = 'C:\\{databaseName}Data.mdf', " +
+                                                    "SIZE = 2MB, MAXSIZE = 10MB, FILEGROWTH = 10%)" +
+                                                    $"LOG ON (NAME = {databaseName}_Log, " +
+                                                    $"FILENAME = 'C:\\{databaseName}Log.ldf', " +
+                                                    "SIZE = 1MB, " +
+                                                    "MAXSIZE = 5MB, " +
+                                                    "FILEGROWTH = 10%)";
+
+                        using (SqlCommand command = new SqlCommand(createDatabaseQuery, connection))
+                        {
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                            Console.WriteLine("Database is created successfully.");
+                            Console.ReadLine();
+                        }
                     }
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error creating database: " + ex.Message);
+                Console.WriteLine("Error creating or checking database: " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+
+
+
+        private static bool CheckIfDatabaseExists(SqlConnection connection, string databaseName)
+        {
+            string query = $"SELECT COUNT(*) FROM master.dbo.sysdatabases WHERE name = '{databaseName}'";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                int databaseCount = Convert.ToInt32(command.ExecuteScalar());
+                return databaseCount > 0;
             }
         }
     }
 }
+
--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,70 +1,74 @@
-﻿// Import the necessary functions from the Windows API
+﻿using Flashcards;
 using System.Runtime.InteropServices;
 
-[DllImport("kernel32.dll", SetLastError = true)]
-static extern IntPtr GetStdHandle(int nStdHandle);
 
-[DllImport("kernel32.dll", SetLastError = true)]
-static extern int SetConsoleOutputCP(uint wCodePageID);
 
-// Set the console font to Lucida Console (or any font that supports Unicode)
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-// Set the console output code page to UTF-8
-SetConsoleOutputCP(65001);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr GetStdHandle(int nStdHandle);
 
-Console.WriteLine("Welcome to Flash Cards your study program.");
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern int SetConsoleOutputCP(uint wCodePageID);
 
-char upArrow = '\u2191';
-char downArrow = '\u2193';
-char checkMark = '\u2713';
+    // Set the console font to Lucida Console (or any font that supports Unicode)
+    Console.OutputEncoding = System.Text.Encoding.UTF8;
+    // Set the console output code page to UTF-8
+    SetConsoleOutputCP(65001);
 
-string black = "\u001b[30m";
-string red = "\u001b[31m";
-string green = "\u001b[32m";
-string yellow = "\u001b[33m";
-string blue = "\u001b[34m";
-string Magenta = "\u001b[35m";
-string Cyan = "\u001b[36m";
-string White = "\u001b[37m";
-string resetColor = "\u001b[0m";
+    Data.CheckCreateDatabase();
 
-Console.WriteLine($@"Use {upArrow} and {downArrow} to navigate and press {green}Enter{resetColor} to select.");
+    Console.WriteLine("Welcome to Flash Cards your study program.");
 
-ConsoleKeyInfo key;
-int option = 1;
-bool isSelected = false;
-(int left, int top) = Console.GetCursorPosition();
-string color = $"{checkMark}{green}   ";
+    char upArrow = '\u2191';
+    char downArrow = '\u2193';
+    char checkMark = '\u2713';
 
-Console.CursorVisible = false;
+    string black = "\u001b[30m";
+    string red = "\u001b[31m";
+    string green = "\u001b[32m";
+    string yellow = "\u001b[33m";
+    string blue = "\u001b[34m";
+    string Magenta = "\u001b[35m";
+    string Cyan = "\u001b[36m";
+    string White = "\u001b[37m";
+    string resetColor = "\u001b[0m";
 
-while (!isSelected)
-{
-    Console.SetCursorPosition(left, top);
+    Console.WriteLine($@"Use {upArrow} and {downArrow} to navigate and press {green}Enter{resetColor} to select.");
 
-    Console.WriteLine($@"{(option == 1 ? color : "    ")}Option 1{resetColor}");
-    Console.WriteLine($@"{(option == 2 ? color : "    ")}Option 2{resetColor}");
-    Console.WriteLine($@"{(option == 3 ? color : "    ")}Option 3{resetColor}");
+    ConsoleKeyInfo key;
+    int option = 1;
+    bool isSelected = false;
+    (int left, int top) = Console.GetCursorPosition();
+    string color = $"{checkMark}{green}   ";
 
-    key = Console.ReadKey(true);
+    Console.CursorVisible = false;
 
-    switch(key.Key)
-
+    while (!isSelected)
     {
-        case ConsoleKey.DownArrow:
-            option = (option == 3 ? 1 : option +1);
-            break;
+        Console.SetCursorPosition(left, top);
 
-        case ConsoleKey.UpArrow:
-            option = (option == 1 ? 3 : option -1);
-            break;
+        Console.WriteLine($@"{(option == 1 ? color : "    ")}Option 1{resetColor}");
+        Console.WriteLine($@"{(option == 2 ? color : "    ")}Option 2{resetColor}");
+        Console.WriteLine($@"{(option == 3 ? color : "    ")}Option 3{resetColor}");
 
-        case ConsoleKey.Enter:
-            isSelected = true;
-            break;
+        key = Console.ReadKey(true);
+
+        switch (key.Key)
+
+        {
+            case ConsoleKey.DownArrow:
+                option = (option == 3 ? 1 : option + 1);
+                break;
+
+            case ConsoleKey.UpArrow:
+                option = (option == 1 ? 3 : option - 1);
+                break;
+
+            case ConsoleKey.Enter:
+                isSelected = true;
+                break;
+        }
+
     }
 
-}
-
-Console.WriteLine($@"You selected option {option}");
+    Console.WriteLine($@"You selected option {option}");
 
