0c52195ae34bf768193a31576beb5386f55632d0
Paul W Saltzman <131216446+Paul-W-Saltzman@users.noreply.github.com>
2023-10-21 20:04:26 -0500

    Added a couple classes.

---
+++ Flashcards/Cards.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards
+{
+    internal class Cards
+    {
+        internal int CardID { get; set; }
+        internal int StackID { get; set; }
+        internal int NoInStack { get; set; }
+        internal string Front { get; set; }
+        internal string Back { get; set; }
+
+    }
+}
